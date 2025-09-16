22e2c6929632b455252b38d74c3a6444cfd33149
accounts15 <accounts15@kodego.ph>
2025-09-16 22:21:52 +0800

    Fix Table issue

--- app/Models/CustomerLocation.php
+++ app/Models/CustomerLocation.php
@@ -10,22 +10,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class CustomerLocation extends Model
 {
     use HasFactory;
-
-    protected $fillable = [
-        'customer_id',
-        'location_name',
-        'address',
-        'city',
-        'state',
-        'zip_code',
-        'country',
-        'contact_person',
-        'contact_phone',
-        'contact_email',
-        'notes',
-        'is_primary',
-        'is_active',
-    ];
+    protected $table = 'customer_locations';
+    protected $guarded = ['id'];
 
     protected $casts = [
         'is_primary' => 'boolean',
